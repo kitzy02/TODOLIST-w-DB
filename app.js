eb7ba06cf280a987f1fccfbc937b9e651d0fe9ad
kitzy02 <142169510+kitzy02@users.noreply.github.com>
2024-07-28 23:20:09 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,45 +1,62 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var app=express();
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/todo");
-const trySchema=new mongoose.Schema({
-    name:String
+const trySchema = new mongoose.Schema({
+    name: String
 });
-const item=mongoose.model("task",trySchema);
-const todo=new item({
-    name:"Create some videos"
+const item = mongoose.model("task", trySchema);
+const todo = new item({
+    name: "Create some videos"
 });
-const todo2=new item({
-    name:"Learn DSA"
+const todo2 = new item({
+    name: "Learn DSA"
 });
-const todo3=new item({
-    name:"Learn React"
+const todo3 = new item({
+    name: "Learn React"
 });
-const todo4=new item({
-    name:"Take some rest"
+const todo4 = new item({
+    name: "Take some rest"
 });
+// todo.save();
 // todo2.save();
 // todo3.save();
 // todo4.save();
 
-
-
-app.get("/",function(req,res){
-    item.find({},function(err,foundItems){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("list",{deyej: foundItems});
-        }
+app.get("/", function(req, res) {
+    item.find({}).then(foundItems => {
+        res.render("list", { deyej: foundItems });
+    }).catch(err => {
+        console.log(err);
     });
 });
+app.post('/', (req, res) => {
+    const itemName=req.body.ele1;
+    const todo5=new item({
+        name:itemName
+    });
+    todo5.save();
+    res.redirect("/");
+    
+});
+app.post('/delete', (req, res) => {
+    const checked = req.body.checkbox1;
+    item.findByIdAndDelete(checked)
+     .then(() => {
+        console.log("deleted");
+        res.redirect("/");
+      })
+     .catch((err) => {
+        console.log(err);
+      });
+  });
 
-app.listen(8000,function(){
+app.listen(800, function() {
     console.log("Server is running");
 });
